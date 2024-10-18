e4e38f7481b1d8f233ec65889857525cad50023c
Justin <justinchen2@outlook.com>
2024-10-19 01:48:14 +1200

    Modification

--- LinkedList.h
+++ LinkedList.h
@@ -7,78 +7,79 @@ using namespace std;
 template <typename T>
 class Node {
 public:
-    T data;
-    Node* next;
+	T data;
+	Node* next;
 
-    Node(T data) {
-        this->data = data;
-        next = NULL;
-    }
+	Node(T data) {
+		this->data = data;
+		next = NULL;
+	}
 
-    ~Node() {}
+	~Node() {}
 };
 
 template <typename T>
 class LinkedList {
 private:
-    Node<T>* head;
+	Node<T>* head;
 
 public:
-    LinkedList(){
-        head = NULL;
-    }
+	LinkedList() {
+		head = NULL;
+	}
 
-    // Insert a new node at the end of the linked list
-    void insert(T data) {
-        Node<T>* newNode = new Node<T>(data);
-        if (!head) {
-            head = newNode;
-        } else {
-            Node<T>* temp = head;
-            while (temp->next) {
-                temp = temp->next;
-            }
-            temp->next = newNode;
-        }
-    }
+	// Insert a new node at the end of the linked list
+	void insert(T data) {
+		Node<T>* newNode = new Node<T>(data);
+		if (!head) {
+			head = newNode;
+		}
+		else {
+			Node<T>* temp = head;
+			while (temp->next) {
+				temp = temp->next;
+			}
+			temp->next = newNode;
+		}
+	}
 
-    // Accessor method to get the head of the linked list
-    Node<T>* getHead() const {
-        return head;
-    }
+	// Accessor method to get the head of the linked list
+	Node<T>* getHead() const {
+		return head;
+	}
 
-    // Find a student by ID
-    T* findById(const string& id) {
-        Node<T>* temp = head;
-        while (temp) {
-            if (temp->data.getId() == id) {
-                return &(temp->data);
-            }
-            temp = temp->next;
-        }
-            return NULL;
-    }
+	// Find a student by ID
+	T* findById(const string& id) {
+		Node<T>* temp = head;
+		while (temp) {
+			if (temp->data.getId() == id) {
+				return &(temp->data);
+			}
+			temp = temp->next;
+		}
+		return NULL;
+	}
 
-    // Print all student data
-    void printAll() const {
-        Node<T>* temp = head;
-        cout << "ID        CW Marks   FE Marks   Total      Grade\n";
-        cout << "----------------------------------------------\n";
-        while (temp) {
-            temp->data.print();
-            temp = temp->next;
-        }
-    }
+	// Print all student data
+	void printAll() const {
+		Node<T>* temp = head;
+		cout << "ID        CW Marks   FE Marks   Total      Grade\n";
+		cout << "----------------------------------------------\n";
+		while (temp) {
+			temp->data.print();
+			temp = temp->next;
+		}
+	}
 
-    // Destructor to clean up memory
-    ~LinkedList() {
-        Node<T>* temp = head;
-        while (temp) {
-            Node<T>* next = temp->next;
-            delete temp;
-            temp = next;
-        }
-    }
+	// Destructor to clean up memory
+	~LinkedList() {
+		Node<T>* temp = head;
+		while (temp) {
+			Node<T>* next = temp->next;
+			delete temp;
+			temp = next;
+		}
+	}
 };
 
 #endif
--- Student.h
+++ Student.h
@@ -7,60 +7,66 @@ using namespace std;
 
 class Student {
 private:
-    string id;
-    double courseworkMarks;
-    double finalExamMarks;
-    double totalMarks;
+	string id;
+	double courseworkMarks;
+	double finalExamMarks;
+	double totalMarks;
 
 public:
-    Student() : id(""), courseworkMarks(0), finalExamMarks(0), totalMarks(0) {}
-    Student(string id, double coursework, double finalExam){
-        this->id = id;
-        this->courseworkMarks = coursework;
-        this->finalExamMarks = finalExam;
-        this->totalMarks = courseworkMarks + finalExamMarks;
-    }
+	Student() {
+		this->id = "";
+		this->courseworkMarks = 0;
+		this->finalExamMarks = 0;
+		this->totalMarks = 0;
+	}
 
-    string getId() const {
-        return id;
-    }
+	Student(string id, double coursework, double finalExam) {
+		this->id = id;
+		this->courseworkMarks = coursework;
+		this->finalExamMarks = finalExam;
+		this->totalMarks = courseworkMarks + finalExamMarks;
+	}
 
-    double getTotalMarks() const {
-        return totalMarks;
-    }
+	string getId() const {
+		return id;
+	}
 
-    char getGrade() const {
-        if (totalMarks >= 80) 
-        {
-            return 'A';
-        }
-        else if (totalMarks >= 65)
-        {
-            return 'B';
-        }
-        else if (totalMarks >= 50)
-        {
-            return 'C';
-        } 
-        else 
-        {
-        return 'F';
-        }
-    }
+	double getTotalMarks() const {
+		return totalMarks;
+	}
 
-    void updateMarks(double coursework, double finalExam) {
-        courseworkMarks = coursework;
-        finalExamMarks = finalExam;
-        totalMarks = courseworkMarks + finalExamMarks;
-    }
+	char getGrade() const {
+		if (totalMarks >= 80)
+		{
+			return 'A';
+		}
+		else if (totalMarks >= 65)
+		{
+			return 'B';
+		}
+		else if (totalMarks >= 50)
+		{
+			return 'C';
+		}
+		else
+		{
+			return 'F';
+		}
+	}
 
-    void print() const {
-        cout << left << setw(10) << id
-             << setw(10) << courseworkMarks
-             << setw(10) << finalExamMarks
-             << setw(10) << totalMarks
-             << setw(5) << getGrade() << endl;
-    }
+	void updateMarks(double coursework, double finalExam) {
+		courseworkMarks = coursework;
+		finalExamMarks = finalExam;
+		totalMarks = courseworkMarks + finalExamMarks;
+	}
+
+	void print() const {
+		cout << left << setw(10) << id
+			<< setw(10) << courseworkMarks
+			<< setw(10) << finalExamMarks
+			<< setw(10) << totalMarks
+			<< setw(5) << getGrade() << endl;
+	}
 };
 
 #endif
