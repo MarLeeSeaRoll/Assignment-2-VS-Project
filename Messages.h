16f4e4774dcf42fce5bffdd97a62a8aedb315958
Justin <justinchen2@outlook.com>
2024-10-19 16:25:18 +1200

    More comments

--- Messages.h
+++ Messages.h
@@ -1,3 +1,5 @@
+/*This is the file including welcome message, exiting message,
+main options menu and options menu for update marks*/
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
@@ -7,36 +9,38 @@
 
 using namespace std;
 
+//Welcome meaasge block
 void printWelcomeMessage()
 {
-    cout << "+-------------------------------------------------+\n";
-    cout << "|      WELCOME TO THE STUDENT GRADES PROGRAM      |\n";
-    cout << "+-------------------------------------------------+\n";
+	cout << "+-------------------------------------------------+\n";
+	cout << "|      WELCOME TO THE STUDENT GRADES PROGRAM      |\n";
+	cout << "+-------------------------------------------------+\n";
 }
 
-void displayMenu(){
-        cout << "\nMenu:\n";
-        cout << "1. Print Student Details\n";
-        cout << "2. Update Student Marks\n";
-        cout << "3. Exit\n";
-        cout << "Enter your choice: ";
+//Menu options
+void displayMenu() {
+	cout << "\nMenu:\n";
+	cout << "1. Print Student Details\n";
+	cout << "2. Update Student Marks\n";
+	cout << "3. Exit\n";
+	cout << "Enter your choice: ";
 }
-
+//Update mark options
+void updateMarkMenu() {
+	cout << "Which Mark Would You Like To Update?" << endl;
+	cout << "1. Coursework Only" << endl;
+	cout << "2. Final Exam Only" << endl;
+	cout << "3. Course and Final Exam" << endl;
+}
+//Exiting message
 void endProgram(bool& continueExecuting)
 {
-    continueExecuting = false;
+	continueExecuting = false;
 
-    cout << endl;
-    cout << "+-------------------------------------+\n";
-    cout << "|  Thank you for using this program!  |\n";
-    cout << "+-------------------------------------+\n";
+	cout << endl;
+	cout << "+-------------------------------------+\n";
+	cout << "|  Thank you for using this program!  |\n";
+	cout << "+-------------------------------------+\n";
 }
 
-void updateMarkMenu(){
-    cout << "Which Mark Would You Like To Update?" << endl;
-    cout << "1. Coursework Only" << endl;
-    cout << "2. Final Exam Only" << endl;
-    cout << "3. Course and Final Exam" << endl;	
-}
-	
 #endif
